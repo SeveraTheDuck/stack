4dd1b6bfcb54b5880990456bb0f7d69901545eb1
SeveratheDuck <antipov.aa@phystech.edu>
2023-09-28 12:58:51 +0300

    Await for hacks

--- main.cpp
+++ main.cpp
@@ -2,47 +2,7 @@
 
 int main()
 {
-    Stack stk1 = {};
-    STACK_CTOR (&stk1);
-
-    StackPush  (&stk1, 10);
-    StackPush  (&stk1, 20);
-    StackPush  (&stk1, 30);
-    StackPush  (&stk1, 40);
-    StackPush  (&stk1, 50);
-    StackPush  (&stk1, 60);
-
-    *((char*)&stk1) = 10;
-
-    int x = 0;
-    StackPop   (&stk1, &x);
-    printf(OUTPUT_F "\n\n", x);
-
-    StackPrint (&stk1);
-
-    StackDtor (&stk1);
-
-///////////////////////////////////////////////////
-
-    Stack stk2 = {};
-    STACK_CTOR (&stk2);
-
-    StackPush  (&stk2, 10);
-    StackPush  (&stk2, 20);
-    StackPush  (&stk2, 30);
-    StackPush  (&stk2, 40);
-    StackPush  (&stk2, 50);
-    StackPush  (&stk2, 60);
-
-
-
-    x = 0;
-    StackPop   (&stk2, &x);
-    printf(OUTPUT_F "\n\n", x);
-
-    StackPrint (&stk2);
-
-    StackDtor  (&stk2);
+    //await your hacks:)
 
     return 0;
 }
