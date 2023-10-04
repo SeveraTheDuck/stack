810e9544a52080a89e8ddc4a734f88edfb80b334
SeveratheDuck <antipov.aa@phystech.edu>
2023-10-05 01:44:01 +0300

    Minor mistakes in docs

---
+++ html/search/functions_8.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['va_5fend_0',['va_end',['../errors_8cpp.html#a36af1f79dd6adf0809c21e76535034d5',1,'errors.cpp']]],
+  ['va_5fstart_1',['va_start',['../errors_8cpp.html#aecf9a9be8105efb1454cd8c1ef6cb1c5',1,'errors.cpp']]],
+  ['vfprintf_2',['vfprintf',['../errors_8cpp.html#a883f70ea2ef453826c529690ee54d388',1,'vfprintf(stderr, fmt, args):&#160;errors.cpp'],['../errors_8cpp.html#affccefce6edf9cc65ac94999550444c3',1,'vfprintf(fp, fmt, args):&#160;errors.cpp']]],
+  ['virtualfunc_3',['virtualfunc',['../class_my_library_1_1_example.html#a8431ef1c10c4e2a36302b8e8556514f6',1,'MyLibrary::Example::virtualfunc()'],['../class_my_library_1_1_subclass_example.html#a08668a9f16511ad37fb547c985d62764',1,'MyLibrary::SubclassExample::virtualfunc()']]]
+];
---
+++ html/search/pages_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['my_20stack_20documentation_0',['My Stack Documentation',['../index.html',1,'']]]
+];
