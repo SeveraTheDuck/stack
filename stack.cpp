781acc2503447ce255d125a52ee438a7346e3191
SeveratheDuck <antipov.aa@phystech.edu>
2023-10-30 22:45:10 +0300

    Solved bug with canaries during allocation

--- stack.cpp
+++ stack.cpp
@@ -104,15 +104,15 @@ ErrorType StackDataAlloc (Stack* const stk, Elem_t* const allocated_memory)
         return stk->stack_err;
     }
 
-    for (size_t i = stk->data_size; i < stk->data_capacity; ++i)
-    {
-        *(stk->data + i) = POISON;
-    }
-
     #ifdef CANARY_PROTECTION
         stk->data = (Elem_t*) ((Canary_t*)(void*) stk->data + 1);
         FillCanary (stk);
     #endif
+
+    for (size_t i = stk->data_size; i < stk->data_capacity; ++i)
+    {
+        *(stk->data + i) = POISON;
+    }
 
     #ifdef HASH_PROTECTION
         StackFindHash     (stk, &stk->hash_value);
